98fc4d38039f5fa2e98fccaeabc6e175b8d6919d
Xavimont0409 <xavier.mont.0409@gmail.com>
2024-03-14 12:36:43 -0500

    new data

---
+++ api/src/controllers/schedule.controller.js
@@ -0,0 +1,11 @@
+const { Student } = require('../db')
+const { errorUser } = require('../utils/error')
+
+const addSchedulreStudent = () => {
+  try {
+
+  } catch (error) {
+    errorUser(error, res)
+  }
+}
+
--- front/src/components/schedule/ModalAddUser.jsx
+++ front/src/components/schedule/ModalAddUser.jsx
@@ -1,12 +1,49 @@
+import DataTable  from 'react-data-table-component'
 import { Modal } from '../../utils/Modal'
-
+import { useStudents } from '../../hooks/studentsHook/useStudents'
 export function ModalAddUser ({ setOpenModalAdd }){
+  const { students } = useStudents()
   const closeModal = () => {
     setOpenModalAdd(false)
   }
+
+  const columns = [
+    {
+      name: <span className=' font-bold'>Nombre</span>,
+      selector: row => row.name,
+      sortable: true,
+      header: 'NOMBRES'
+    },
+    {
+      name: <span className=' font-bold'>Apellido</span>,
+      selector: row => row.lastName,
+      sortable: true
+    },
+    {
+      name: <span className=' font-bold'>Categoria</span>,
+      selector: row => row.CategoryName,
+      sortable: true
+    },
+    {
+      name: <span className=' font-bold'>Nivel</span>,
+      selector: row => row.LevelName,
+      sortable: true
+    }
+  ]
+
   return (
     <Modal title='Agregar estudiante' closeModal={closeModal} className='flex flex-col justify-between bg-white rounded-md w-2/4 px-5 pb-5' >
-      <h1>soy un modal</h1>
+      <DataTable 
+        title='Alumnos sin horarios'
+        columns={columns}
+        data={students}
+
+        striped
+        responsive
+        pagination
+        highlightOnHover
+        paginationServer
+      />
     </Modal>
   )
 }
